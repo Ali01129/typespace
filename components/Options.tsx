2f71d7d7194efc19c2af65525f9c464aaf1539bf
ali01129 <chaudharyalinawazz@gmail.com>
2025-10-04 22:41:03 +0500

    Fixed Navbar and activated shortcut

--- components/Options.tsx
+++ components/Options.tsx
@@ -1,35 +1,31 @@
 "use client";
 
-import { useEffect } from "react";
+import { toggleFullscreen, clearPage } from "../hooks/useKeyboardShortcuts";
+
 
 export default function Options() {
-  const toggleFullscreen = () => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
-  };
 
   return (
     <div className="absolute right-0 mt-2 w-60 px-2 py-2 bg-black border border-gray-400 rounded-md shadow-lg z-50">
       
-      <button className="flex justify-between items-center w-full text-left px-4 py-2 text-xs rounded-md hover:bg-[#262626]">
+      <button className="flex justify-between items-center w-full text-left px-4 py-2 text-xs rounded-md hover:bg-[#262626]"
+      onClick={clearPage}
+      >
         <span className="text-white font-mono">Clear page</span>
         <span className="text-gray-400 font-mono">ctrl+alt+c</span>
       </button>
       
+      <button className="flex justify-between items-center w-full text-left px-4 py-2 text-xs rounded-md hover:bg-[#262626]">
+        <span className="text-white font-mono">Light theme</span>
+        <span className="text-gray-400 font-mono">ctrl+alt+l</span>
+      </button>
+
       <button
         className="flex justify-between items-center w-full text-left px-4 py-2 text-xs rounded-md hover:bg-[#262626]"
         onClick={toggleFullscreen}
       >
         <span className="text-white font-mono">Full screen</span>
-        <span className="text-gray-400 font-mono">ctrl+alt+f</span>
-      </button>
-
-      <button className="flex justify-between items-center w-full text-left px-4 py-2 text-xs rounded-md hover:bg-[#262626]">
-        <span className="text-white font-mono">Light theme</span>
-        <span className="text-gray-400 font-mono">ctrl+alt+l</span>
+        <span className="text-gray-400 font-mono">F11</span>
       </button>
 
       <button className="block w-full text-left px-4 py-2 text-xs font-mono rounded-md hover:bg-[#262626]">
---
+++ hooks/useKeyboardShortcuts.ts
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { zustandStore } from '../zustand/store';
+
+export const useKeyboardShortcuts = () => {
+  const { setNote } = zustandStore();
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+        const isCtrlOrCmd = e.ctrlKey || e.metaKey;
+
+        if (isCtrlOrCmd && e.altKey && e.key.toLowerCase() === "c") {
+          e.preventDefault();
+          clearPage();
+        }
+      
+      if (e.key === "F11") {
+        e.preventDefault();
+        toggleFullscreen();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setNote]);
+
+};
+
+export const clearPage = () => {
+    zustandStore.getState().setNote("");
+};
+
+export const toggleFullscreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+};
