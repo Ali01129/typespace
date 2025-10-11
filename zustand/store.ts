8ecc7c64ff41fbbd129ddf82e81386cc9eba1218
ali01129 <chaudharyalinawazz@gmail.com>
2025-10-11 14:56:31 +0500

    Modal: Added token in options

--- zustand/store.ts
+++ zustand/store.ts
@@ -3,9 +3,19 @@ import { create } from 'zustand';
 type store = {
   note: string
   setNote: (note: string) => void;
+
+  token: number
+  setToken: (token:number) => void;
+  addToken: () => void;
+  subToken: () => void;
 };
 
 export const zustandStore = create<store>((set) => ({
   note: "",
   setNote: (note: string) => set({ note }),
+
+  token: 0,
+  setToken: (token: number) => set({token}),
+  addToken: () => set((state) => ({ token: state.token + 1 })),
+  subToken: () => set((state) => ({ token: state.token - 1 })),
 }));
